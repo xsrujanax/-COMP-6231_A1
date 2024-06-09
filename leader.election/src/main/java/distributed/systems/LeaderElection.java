be1e203877b108b3c4cebd3b2f4448f81063b31d
Srujana Guttula <srujana.guttula@outlook.com>
2024-06-09 12:59:37 -0400

    added exception handling

--- leader.election/src/main/java/distributed/systems/LeaderElection.java
+++ leader.election/src/main/java/distributed/systems/LeaderElection.java
@@ -3,11 +3,12 @@ package distributed.systems;
 import org.apache.zookeeper.*;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Handles leader election for a distributed system using Apache ZooKeeper.
+ */
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
@@ -15,10 +16,25 @@ public class LeaderElection implements Watcher {
     private ZooKeeper zooKeeper;
     private String currentZnodeName;
 
+    /**
+     * Establishes a connection to the ZooKeeper server.
+     * @throws IOException if an I/O error occurs when attempting to connect.
+     */
     public void connectToZookeeper() throws IOException {
-        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        try {
+            this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+            System.out.println("Connecting to Zookeeper at " + ZOOKEEPER_ADDRESS);
+        } catch (IOException e) {
+            System.out.println("Failed to connect to Zookeeper");
+            throw e;
+        }
     }
 
+    /**
+     * Processes ZooKeeper events related to the connection state.
+     *
+     * @param event The watched event that occurred.
+     */
     @Override
     public void process(WatchedEvent event){
         switch (event.getType()){
@@ -35,34 +51,76 @@ public class LeaderElection implements Watcher {
         }
     }
 
+    /**
+     * Main method that keeps the application running, waiting for the connection to be closed.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting.
+     */
     public void run() throws InterruptedException {
         synchronized (zooKeeper){
-            zooKeeper.wait();
+            try {
+                zooKeeper.wait();
+            }catch (InterruptedException e){
+                System.out.println("Thread interrupted");
+                throw e;
+            }
         }
     }
 
+    /**
+     * Closes the ZooKeeper connection.
+     *
+     * @throws InterruptedException if the thread is interrupted during the process.
+     */
     public void close() throws InterruptedException {
-        zooKeeper.close();
-    }
-
-    public void volunteerForLeadership() throws KeeperException, InterruptedException{
-        String znodePrefix = ELECTION_NAMESPACE + "/c_";
-        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
-
-        System.out.println("znode name" + znodeFullPath);
-        this.currentZnodeName = znodeFullPath.replace("/election/","");
-    }
-
-    public void electLeader() throws  KeeperException, InterruptedException{
-        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
-        Collections.sort(children);
-        String smallestChild = children.get(0);
-
-        if(smallestChild.equals(currentZnodeName)){
-            System.out.println("I'm the leader");
-            return;
+        try {
+            zooKeeper.close();
+            System.out.println("ZooKeeper connection closed");
+        } catch (InterruptedException e) {
+            System.out.println("Failed to close ZooKeeper connection");
         }
-        System.out.println("Im not the leader," + smallestChild+" is the leader");
+    }
+
+    /**
+     * Volunteers this instance for leadership by creating a znode in ZooKeeper.
+     *
+     * @throws KeeperException if there is an error interacting with ZooKeeper.
+     * @throws InterruptedException if the thread is interrupted.
+     */
+    public void volunteerForLeadership() throws KeeperException, InterruptedException{
+
+        try{
+            String znodePrefix = ELECTION_NAMESPACE + "/c_";
+            String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+            System.out.println("znode created" + znodeFullPath);
+            this.currentZnodeName = znodeFullPath.replace("/election/","");
+        } catch (KeeperException | InterruptedException e) {
+            System.out.println("Failed to volunteer for leadership");
+            throw e;
+        }
+    }
+
+    /**
+     * Volunteers this instance for leadership by creating a znode in ZooKeeper.
+     *
+     * @throws KeeperException if there is an error interacting with ZooKeeper.
+     * @throws InterruptedException if the thread is interrupted.
+     */
+    public void electLeader() throws  KeeperException, InterruptedException{
+        try{
+            List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+            Collections.sort(children);
+            String smallestChild = children.get(0);
+
+            if(smallestChild.equals(currentZnodeName)){
+                System.out.println("I'm the leader");
+                return;
+            }
+            System.out.println("Im not the leader," + smallestChild+" is the leader");
+        } catch (KeeperException | InterruptedException e){
+            System.out.println("Failed during the election process");
+            throw e;
+        }
 
     }
 }
